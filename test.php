df7976127244a803a6e8cb0a752ced20f8856463
Ricardo Bermúdez Bermúdez <ricardob.sistemas@gmail.com>
2020-01-10 16:28:32 -0600

    Se separa operadores mágicos de la clase principal

--- test.php
+++ test.php
@@ -1,26 +1,11 @@
 <?php
 
 class TestParent {
-    public function getTestVar(){
-        return $this->testVar;
-    }
-}
-
-class Test extends TestParent
-{
     public $testVar = 0;
     
     public function __construct() {
         echo "Hi from constructor\n";
         $this->testVar = 11;
-    }
-    
-    public function getTestVarPlus($x) {
-        return $this->testVar+$x;
-    }
-    
-    protected function getInitial() {
-        return 0;
     }
     
     public function __call($fnName, $fnArgs) {
@@ -35,6 +20,21 @@ class Test extends TestParent
     
     public function __toString() {
         return "to string method";
+    }
+    
+    public function getTestVar(){
+        return $this->testVar;
+    }
+}
+
+class Test extends TestParent
+{   
+    public function getTestVarPlus($x) {
+        return $this->testVar+$x;
+    }
+    
+    protected function getInitial() {
+        return 0;
     }
 }
 
